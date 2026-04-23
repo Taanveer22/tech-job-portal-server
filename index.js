da37020108233c6f66c4883179f02ba392e57a83
Taanveer22 <taanveer469@gmail.com>
2026-04-23 10:38:38 +0600

    alhamdulliah db setup done and manual db and coll added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-//Instance Initialization
+//App Setup
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,11 +11,50 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//Server Run
+// Database Setup
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    console.log("mongodb connected");
+    // ###########################################################
+    const database = client.db("jobsDB");
+    const jobsCollection = database.collection("jobsColl");
+
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // ###########################################################
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
+
+//Root Route
 app.get("/", (req, res) => {
   res.send("server is running");
 });
 
+//Server Start
 app.listen(PORT, () => {
   console.log(`this sever is running on port no : ${PORT}`);
 });
