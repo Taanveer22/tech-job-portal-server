fd8e775175e2e20fb67ed594da0c7f31b5a47d95
Taanveer22 <taanveer469@gmail.com>
2026-05-04 22:40:08 +0600

    alhamdulliah set token to the cookie

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware Setup
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -45,7 +50,7 @@ async function run() {
         .cookie('token', token, {
           httpOnly: true,
           secure: false,
-          sameSite: 'none',
+          sameSite: 'lax',
         })
         .send({ success: true });
     });
